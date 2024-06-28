c8b404915cf52b173d2b05354f2ba1280dc4027a
EunbeenDev <ros4312@naver.com>
2024-06-29 04:44:49 +0900

    날씨에 따른 이미지 추천 기능 구현

--- src/main/java/com/summerpics/domain/weather_pics/presentation/WeatherPicsController.java
+++ src/main/java/com/summerpics/domain/weather_pics/presentation/WeatherPicsController.java
@@ -1,12 +1,30 @@
 package com.summerpics.domain.weather_pics.presentation;
 
+import com.summerpics.domain.weather_pics.application.WeatherPicsService;
+import com.summerpics.domain.weather_pics.dto.request.WeatherInfoReq;
+import com.summerpics.domain.weather_pics.dto.response.WeatherPicRes;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/pic")
 public class WeatherPicsController {
+
+    private final WeatherPicsService weatherPicsService;
+
+    @PostMapping("/")
+    public ResponseEntity<WeatherPicRes> getWeatherPic(@RequestBody WeatherInfoReq request) throws IOException, ParseException {
+        WeatherPicRes weatherPicRes = weatherPicsService.getWeatherPic(request);
+        return ResponseEntity.ok(weatherPicRes);
+    }
 }
+
