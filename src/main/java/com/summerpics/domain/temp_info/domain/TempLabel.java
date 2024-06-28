9456557e136ed1e0452c079bfd30b9a376d1ccc3
EunbeenDev <ros4312@naver.com>
2024-06-29 02:33:07 +0900

    도메인 수정

--- src/main/java/com/summerpics/domain/temp_info/domain/TempLabel.java
+++ src/main/java/com/summerpics/domain/temp_info/domain/TempLabel.java
@@ -1,4 +1,16 @@
 package com.summerpics.domain.temp_info.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum TempLabel {
+    LEVEL0("0"),
+    LEVEL1("1"),
+    LEVEL2("2"),
+    LEVEL3("3"),
+    LEVEL4("4");
+
+    private String value;
 }
