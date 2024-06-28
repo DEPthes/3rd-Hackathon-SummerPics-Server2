258e2efeb1ed6196d05f81d7630ad65044ec6ce1
EunbeenDev <ros4312@naver.com>
2024-06-28 15:58:13 +0900

    initial commit

---
+++ src/test/java/com/summerpics/SummerPicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summerpics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerPicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
